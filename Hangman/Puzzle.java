c4e9fddbd4954e9503c96f0a05018214a1411da6
Dwdw621 <55766994+Dwdw621@users.noreply.github.com>
2021-11-23 10:18:12 -0800

    Add files via upload

--- Hangman/Puzzle.java
+++ Hangman/Puzzle.java
@@ -10,7 +10,7 @@ public class Puzzle
     private String wrong;
 
     public Puzzle(){
-        word = "kowabunga";
+        word = "HELLOWORLD";
         unknown = "";
         wrong = "";
         for(int i = 0; i != word.length(); i++){
@@ -19,7 +19,9 @@ public class Puzzle
     }
 
     public boolean isUnsolved(){
-        return true;
+        if(unknown.contains("_"))
+            return true;
+        return false;
     }
 
     public void show(){ 
@@ -29,17 +31,16 @@ public class Puzzle
     } 
 
     public boolean makeGuess(String guess){
+        guess = guess.toUpperCase();
         if(word.contains(guess)){
             for(int i = 0; i < word.length(); i++){
                 if(word.substring(i, i+1).equals(guess)){
-                    unknown.replace
+                    unknown = unknown.substring(0, i) + guess + unknown.substring(i+1);
                 }
             }
-
         } else {
             wrong += guess + ", ";
         }
-
         return word.contains(guess);
     }
 
@@ -47,3 +48,4 @@ public class Puzzle
         return word;
     }
 }
+
